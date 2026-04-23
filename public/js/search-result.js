d3907265c9e7e758fcef130cf7d491f4fa9a5e25
Mehzabin <mehzabin@example.com>
2026-04-23 06:28:39 +0600

    M

--- public/js/search-result.js
+++ public/js/search-result.js
@@ -94,12 +94,11 @@ document.addEventListener("DOMContentLoaded", async () => {
     products.forEach((item, index) => {
 
     const img = item.image
-        ? `${BASE_URL}/uploads/${item.image}`
-        : "images/default.png";
-
+  ? `${BASE_URL}/${item.image.replace(/^\/?uploads\//, "uploads/")}`
+  : "images/default.png";
 
       container.innerHTML += `
-        <div style="border:1px solid #ccc; padding:10px; margin:10px;">
+        <div style="border:1px solid #ccc; padding:10px; margin:10px;" id="cat-btn">
           
           <img src="${img}" width="150">
           <h4>${item.name}</h4>
